0c0a4cafa8740ab6eb23e553314065e5addaa230
Imma7 <immamugambi@gmail.com>
2018-08-02 11:39:39 +0300

    Return string representation of the model

--- hood/models.py
+++ hood/models.py
@@ -1,13 +1,29 @@
 from django.db import models
+from django.contrib.gis.db import models
+
 
 # Create your models here.
 class User(models.Model):
     username = models.CharField()
     email = models.EmailField()
-    neighbourHood = models.ForeignKey(NeighbourHood)
+    neighbourhood = models.ForeignKey(NeighbourHood)
+
+    def __str__(self):
+        return self.username
 
 class Business(models.Model):
     business_name = models.CharField()
     business_email = models.EmailField()
     user = models.ForeignKey(User)
-    neighbourHood = models.ForeignKey(NeighbourHood)
+    neighbourhood = models.ForeignKey(NeighbourHood)
+
+    def __str__(self):
+        return self.business_name
+
+class NeighbourHood(models.Model):
+    neighbourhood_name = models.CharField()
+    neighbourhood_location = models.PointField()
+    occupants = models.IntegerField()
+
+    def __str__(self):
+        return self.neighbourhood_name
