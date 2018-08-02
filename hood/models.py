2e64a6099e19186e6613397bec75b7808ef43341
Imma7 <immamugambi@gmail.com>
2018-08-02 10:57:58 +0300

    Added the business model class

--- hood/models.py
+++ hood/models.py
@@ -5,3 +5,9 @@ class User(models.Model):
     username = models.CharField()
     email = models.EmailField()
     neighbourHood = models.ForeignKey(NeighbourHood)
+
+class Business(models.Model):
+    business_name = models.CharField()
+    business_email = models.EmailField()
+    user = models.ForeignKey(User)
+    neighbourHood = models.ForeignKey(NeighbourHood)
