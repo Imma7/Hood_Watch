a285a6c8d38d37c27ee92be2387da06533914e8f
Imma7 <immamugambi@gmail.com>
2018-08-03 16:14:20 +0300

    Added neighborhood class

--- hood/models.py
+++ hood/models.py
@@ -1,18 +1,28 @@
 from django.db import models
-from django.contrib.gis.db import models
-
+from django.contrib.auth.models import User
 
 # Create your models here.
-class User(models.Model):
-    username = models.CharField()
-    email = models.EmailField()
-    neighbourhood = models.ForeignKey(NeighbourHood)
+class NeighbourHood(models.Model):
+    neighbourhood_name = models.CharField(max_length = 50, null=True, blank=False)
+    neighbourhood_location = models.CharField(max_length = 50, null=True, blank=False)
+    occupants = models.IntegerField(null=True, blank=False)
+    user = models.ForeignKey(User, null=True, blank=False)
+
+    def __str__(self):
+        return self.neighbourhood_name
+
+class Profile(models.Model):
+    username = models.CharField(max_length = 50, null=True, blank=False)
+    email = models.EmailField(max_length=100, null=True, blank=False)
+    neighbourhood = models.ForeignKey(NeighbourHood, null=True, blank=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE ,related_name='profile_for') 
+
 
     def __str__(self):
         return self.username
 
 class Business(models.Model):
-    business_name = models.CharField()
+    business_name = models.CharField(max_length = 50)
     business_email = models.EmailField()
     user = models.ForeignKey(User)
     neighbourhood = models.ForeignKey(NeighbourHood)
@@ -20,10 +30,9 @@ class Business(models.Model):
     def __str__(self):
         return self.business_name
 
-class NeighbourHood(models.Model):
-    neighbourhood_name = models.CharField()
-    neighbourhood_location = models.PointField()
-    occupants = models.IntegerField()
 
-    def __str__(self):
-        return self.neighbourhood_name
+
+
+
+
+
