48b0f2d8e39da2618db4691a93045511ec6b8b51
Imma7 <immamugambi@gmail.com>
2018-08-06 18:30:39 +0300

    Created account activation urls

--- hood/urls.py
+++ hood/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name = 'home'),
-    url(r'^signup/$', views.signup, name = 'signup'),
+    url(r'^account_activation_sent/$', core_views.account_activation_sent, name='account_activation_sent'),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        views.activate, name='activate'),
+        core_views.activate, name='activate'),
 ]
