344db5e2753aacd8837701e81b3041148a5b47b1
Imma7 <immamugambi@gmail.com>
2018-08-06 17:54:19 +0300

    Created update user profile function

--- hood/models.py
+++ hood/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class NeighbourHood(models.Model):
@@ -21,6 +23,12 @@ class Profile(models.Model):
     def __str__(self):
         return self.username
 
+@receiver(post_save, sender = User)
+def update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
+
 class Business(models.Model):
     business_name = models.CharField(max_length = 50)
     business_email = models.EmailField()
@@ -29,8 +37,6 @@ class Business(models.Model):
 
     def __str__(self):
         return self.business_name
-
-
 
 
 
