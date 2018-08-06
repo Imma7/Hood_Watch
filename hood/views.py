4a1ab591fb77fa61994554a5c0c35ba55869648f
Imma7 <immamugambi@gmail.com>
2018-08-06 18:14:43 +0300

    Created signup form

--- hood/views.py
+++ hood/views.py
@@ -9,6 +9,8 @@ from django.template.loader import render_to_string
 from .tokens import account_activation_token
 from django.contrib.auth.models import User
 from django.core.mail import EmailMessage
+from mysite.core.forms import SignUpForm
+from mysite.core.tokens import account_activation_token
 
 # Create your views here.
 def signup(request):
@@ -19,35 +21,15 @@ def signup(request):
             user.is_active = False
             user.save()
             current_site = get_current_site(request)
-            mail_subject = 'Activate your blog account.'
-            message = render_to_string('acc_active_email.html', {
+            subject = 'Activate Your Hood Account'
+            message = render_to_string('account_activation_email', {
                 'user': user,
                 'domain': current_site.domain,
-                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
-                'token':account_activation_token.make_token(user),
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
             })
-            to_email = form.cleaned_data.get('email')
-            email = EmailMessage(
-                        mail_subject, message, to=[to_email]
-            )
-            email.send()
-            return HttpResponse('Please confirm your email address to complete the registration')
-    else:
-        form = SignupForm()
-    return render(request, 'signup.html', {'form': form})
-
-
-def activate(request, uidb64, token):
-    try:
-        uid = force_text(urlsafe_base64_decode(uidb64))
-        user = User.objects.get(pk=uid)
-    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
-        user = None
-    if user is not None and account_activation_token.check_token(user, token):
-        user.is_active = True
-        user.save()
-        login(request, user)
-        # return redirect('home')
-        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
-    else:
-        return HttpResponse('Activation link is invalid!')
+            user.email_user(subject, message)
+            return redirect('account_activation_sent')
+        else:
+            form = SignupForm()
+        return render(request, 'signup.html', {'form':form})
