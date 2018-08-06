99a010571b8b776c0ddfb6fb0eb9acf510e73c2c
Imma7 <immamugambi@gmail.com>
2018-08-06 18:06:50 +0300

    Created signup form

--- hood/forms.py
+++ hood/forms.py
@@ -2,8 +2,9 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-class SignupForm(UserCreationForm):
-    email = forms.EmailField(max_length=200, help_text='Required')
+class SIgnUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'email', 'password1', 'password2',)
