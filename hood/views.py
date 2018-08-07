52a02676d920be832c8305fc0e6037a868198c54
Imma7 <immamugambi@gmail.com>
2018-08-07 13:23:39 +0300

    Created index view function

--- hood/views.py
+++ hood/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import login, authenticate
-from .forms import SignupForm
+from .forms import SIgnUpForm
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
@@ -9,27 +9,10 @@ from django.template.loader import render_to_string
 from .tokens import account_activation_token
 from django.contrib.auth.models import User
 from django.core.mail import EmailMessage
-from mysite.core.forms import SignUpForm
-from mysite.core.tokens import account_activation_token
+from .tokens import account_activation_token
+import datetime as dt
 
 # Create your views here.
-def signup(request):
-    if request.method == 'POST':
-        form = SignupForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.is_active = False
-            user.save()
-            current_site = get_current_site(request)
-            subject = 'Activate Your Hood Account'
-            message = render_to_string('account_activation_email', {
-                'user': user,
-                'domain': current_site.domain,
-                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                'token': account_activation_token.make_token(user),
-            })
-            user.email_user(subject, message)
-            return redirect('account_activation_sent')
-        else:
-            form = SignupForm()
-        return render(request, 'signup.html', {'form':form})
+def index(request):
+    date = dt.date.today()
+    return render(request, 'index.html', {'date':date})
