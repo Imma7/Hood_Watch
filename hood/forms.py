1b211a19228698d8f210e29631885b7ba8214b80
Imma7 <immamugambi@gmail.com>
2018-08-08 15:02:03 +0300

    Created NeighbourHood Form

--- hood/forms.py
+++ hood/forms.py
@@ -6,3 +6,8 @@ from django import forms
 class BusinessForm(forms.Form):
     business_name = forms.CharField(label='Business Name', max_length=50)
     business_email = forms.EmailField(label='Email')
+
+class NeighbourHoodForm(forms.Form):
+    neighbourhood_name = forms.CharField(label='Neighbourhood Name', max_length = 50)
+    neighbourhood_location = forms.CharField(label='Location', max_length = 50)
+    occupants = forms.IntegerField(label='Occupants')
