007d155bcf47c356e2954233cea3f70a0c6dd8a7
Imma7 <immamugambi@gmail.com>
2018-08-08 04:03:00 +0300

    Created search urls

--- hood/urls.py
+++ hood/urls.py
@@ -5,4 +5,5 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^search/', views.search_results, name = 'search_results'),
 ]
