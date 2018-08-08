fdd56635034e480e339b23fb1e41930674f939ec
Imma7 <immamugambi@gmail.com>
2018-08-08 14:53:20 +0300

    Created Business Form

--- hood/forms.py
+++ hood/forms.py
@@ -1,10 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django import forms
 
-class SIgnUpForm(UserCreationForm):
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
-
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'password1', 'password2',)
+class BusinessForm(forms.Form):
+    business_name = forms.CharField(label='Business Name', max_length=50)
+    business_email = forms.EmailField(label='Email')
