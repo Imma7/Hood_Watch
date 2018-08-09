f7e714af20727ab353939b03ba7862fe5c44ca6f
Imma7 <immamugambi@gmail.com>
2018-08-09 23:52:41 +0300

    Added specific hood urls

--- hood/urls.py
+++ hood/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 from . import views
 from django.contrib.auth import views as auth_views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
@@ -10,4 +11,8 @@ urlpatterns = [
     url(r'^business/(?P<hood_id>\d+)', views.business, name='business'),
     url(r'^new/post$', views.new_biz, name='new_biz'),
     url(r'^new/profile$', views.new_profile, name='new-profile'),
+    url(r'^biz_in_hood/(?P<neighbourhood_id>\d+)', views.biz_in_hood, name='biz_in_hood'),
+
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
