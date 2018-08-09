6e801ff883f5fd013bda6c87fb2f7aeb71863eba
Imma7 <immamugambi@gmail.com>
2018-08-10 01:17:01 +0300

    Registered Post model

--- hood/admin.py
+++ hood/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from hood.models import Business, NeighbourHood, Profile
+from hood.models import Business, NeighbourHood, Profile, Post
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Business)
 admin.site.register(NeighbourHood)
+admin.site.register(Post)
