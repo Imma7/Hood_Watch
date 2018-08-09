a3e6c176f21ba0d2bf9b1a5ca2f0861e57c7eff1
Imma7 <immamugambi@gmail.com>
2018-08-10 01:06:41 +0300

    Created new post urls

--- hood/urls.py
+++ hood/urls.py
@@ -9,9 +9,11 @@ urlpatterns = [
     url(r'^search/', views.search_results, name = 'search_results'),
     url(r'^hood/(?P<id>\d+)', views.hood, name='hood'),
     url(r'^business/(?P<hood_id>\d+)', views.business, name='business'),
-    url(r'^new/post$', views.new_biz, name='new_biz'),
+    url(r'^new/hood$', views.new_biz, name='new_biz'),
     url(r'^new/profile$', views.new_profile, name='new-profile'),
     url(r'^biz_in_hood/(?P<neighbourhood_id>\d+)', views.biz_in_hood, name='biz_in_hood'),
+    url(r'^new/post$', views.new_post, name='new-post'),
+
 
 ]
 if settings.DEBUG:
