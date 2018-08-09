d6040a50c978847fe27d5ba0319e573021e766a1
Imma7 <immamugambi@gmail.com>
2018-08-09 15:09:59 +0300

    Added account urls

--- hooder/urls.py
+++ hooder/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include ('hood.urls')),
-    url(r'^accounts/', include('registration.backends.hmac.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
