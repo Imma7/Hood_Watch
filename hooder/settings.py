a1c4c7acfd6403a530ecb20305cb798cd5e66363
Imma7 <immamugambi@gmail.com>
2018-08-09 18:07:11 +0300

    deployment

--- hooder/settings.py
+++ hooder/settings.py
@@ -80,15 +80,15 @@ WSGI_APPLICATION = 'hooder.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 if config('MODE')=="dev":
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': '',
-    }
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST'),
+            'PORT': '',
+        }
 }
 # production
 else:
