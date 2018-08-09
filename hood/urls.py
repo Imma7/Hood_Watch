e3c364fb4cf3c0a6bd169548af5a40799739b87f
Imma7 <immamugambi@gmail.com>
2018-08-09 16:35:54 +0300

    Added new profiles url

--- hood/urls.py
+++ hood/urls.py
@@ -6,4 +6,8 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^search/', views.search_results, name = 'search_results'),
+    url(r'^hood/(?P<id>\d+)', views.hood, name='hood'),
+    url(r'^business/(?P<hood_id>\d+)', views.business, name='business'),
+    url(r'^new/post$', views.new_biz, name='new_biz'),
+    url(r'^new/profile$', views.new_profile, name='new-profile'),
 ]
